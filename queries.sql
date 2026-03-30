0cf1d8366a42cf88485f8e694dce7827abad722a
te0dor19 <fedzhur.124@inbox.ru>
2026-03-30 16:06:02 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,3 +2,5 @@
 SELECT COUNT(customerid) AS number_of_customers FROM customer;
 -- 2. Определение общей выручки
 SELECT SUM(total) AS total_revenue FROM invoice;
+-- 3. Находим общее число заказов
+SELECT COUNT(*) AS number_of_orders FROM invoice;
