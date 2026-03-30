085c7acf66792c1b136d6129946a58c2541ffe3b
te0dor19 <fedzhur.124@inbox.ru>
2026-03-30 15:50:46 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,2 +1,4 @@
 -- 1. Общее число пользователей сервиса 
 SELECT COUNT(customerid) AS number_of_customers FROM customer;
+-- 2. Определение общей выручки
+SELECT SUM(total) AS total_revenue FROM invoice;
