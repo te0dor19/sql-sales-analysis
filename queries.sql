fd48480425e1336d18e88df35fbfaaa1fe28d403
te0dor19 <fedzhur.124@inbox.ru>
2026-03-30 16:37:18 +0300

    Update queries.sql
    Количество заказов по дням

--- queries.sql
+++ queries.sql
@@ -11,3 +11,6 @@ SELECT MAX(total) AS max_sum_order FROM invoice;
 -- 6. Считааем выручку по дням (с начала ведения записи)
 SELECT SUM(total) AS day_revenue, invoicedate::date AS invoice_date FROM invoice
 GROUP BY invoice_date ORDER BY invoice_date ASC;
+-- 7. Считаем количество заказов по дням 
+SELECT COUNT(invoiceid) AS orders_number_per_date, invoicedate::date AS invoice_date FROM invoice
+GROUP BY invoice_date ORDER BY invoice_date ASC;
