889e68af7768af9242805e2d216119e71ecaea90
te0dor19 <fedzhur.124@inbox.ru>
2026-03-30 16:21:30 +0300

    Update queries.sql
    Выявляем минимальную сумму заказа

--- queries.sql
+++ queries.sql
@@ -4,3 +4,5 @@ SELECT COUNT(customerid) AS number_of_customers FROM customer;
 SELECT SUM(total) AS total_revenue FROM invoice;
 -- 3. Находим общее число заказов
 SELECT COUNT(*) AS number_of_orders FROM invoice;
+-- 4. Выявляем минимальную сумму заказа 
+SELECT MIN(total) AS min_sum_order FROM invoice;
