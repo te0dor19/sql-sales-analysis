a577a3f23c512d99ddfdcebdec76d2638bbd81ea
te0dor19 <fedzhur.124@inbox.ru>
2026-03-31 22:44:15 +0300

    Update queries.sql
    Последние метрики для проекта

--- queries.sql
+++ queries.sql
@@ -22,3 +22,35 @@ GROUP BY invoice_date ORDER BY invoice_date ASC;
 -- 9. Количество активных клиентов по странам 
 SELECT COUNT(customerid) AS active_number_of_customers, billingcountry AS country
 FROM invoice GROUP BY country ORDER BY country ASC;
+--10. Количество заказов по странам
+SELECT COUNT(invoiceid) AS number_of_orders, billingcountry AS country FROM
+invoice GROUP BY country ORDER BY country;
+-- 11. Находим выручку по странам 
+SELECT SUM(total) AS revenue, billingcountry AS country FROM invoice
+GROUP BY country ORDER BY country;
+-- 12. Выявляем средний чек по странам
+SELECT AVG(total) AS average_revenue, billingcountry as country FROM invoice
+GROUP BY country ORDER BY country;
+-- 13. Вычисляем топ 10 процентов пользователей сервиса по выручке
+SELECT customerid, SUM(total) AS revenue FROM invoice GROUP BY customerid
+ORDER BY revenue DESC, customerid ASC LIMIT 0.1 * 
+(SELECT COUNT(DISTINCT customerid) FROM invoice);
+-- 14. Вычисляем топ 10 процентов пользователей сервиса по числу заказов
+SELECT customerid, COUNT(invoiceid) AS number_of_orders FROM invoice GROUP BY customerid
+ORDER BY number_of_orders DESC, customerid ASC LIMIT 0.1 * 
+(SELECT COUNT(DISTINCT customerid) FROM invoice);
+-- 15. Считаем ARPU (среднюю выручку на каждого пользователя)
+SELECT SUM(total) / COUNT(DISTINCT customerid)
+AS "ARPU" FROM invoice;
+-- 16. Делаем сегментацию по группам пользователей относительно количества заказов и выводим общую выручку группы, количество пользователей группы, категорию группы и среднюю выручку на пользователя в группе
+WITH customer_order_info AS (SELECT SUM(total) AS total_revenue, COUNT(invoiceid) AS number_of_orders,
+customerid FROM invoice GROUP BY customerid),
+client_groups AS (SELECT SUM(total_revenue) AS total_group_revenue, 
+COUNT(customerid) AS number_of_people_in_group,
+CASE 
+	WHEN number_of_orders <= 1 THEN '1 order group'
+	WHEN number_of_orders >= 2 AND number_of_orders <= 5 THEN '2-5 order group'
+	ELSE '6+ order group'
+END AS order_group FROM customer_order_info GROUP BY order_group)
+SELECT total_group_revenue, number_of_people_in_group, order_group,
+total_group_revenue / number_of_people_in_group AS avg_user_revenue_in_group FROM client_groups;
